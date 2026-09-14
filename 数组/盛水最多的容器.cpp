73fac168457016407a56abdcbc2684451c8c4b8a
4RainyMaple <z1975917270@qq.com>
2026-09-14 11:29:06 +0800

    Create 盛水最多的容器.cpp

---
+++ 数组/盛水最多的容器.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int area = 0;
+        int max = 0;
+        while (left != right)
+        {
+            area = (right - left) * min( height[left], height[right] );
+            if ( area > max )
+                max = area;
+            if ( height[left] <= height[right] )
+                ++left;
+            else
+                --right;
+        }
+        return max;
+    }
+};
