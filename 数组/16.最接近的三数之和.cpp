4f1e5a2fe229a25e3c8db8303742d8a5c6b893f5
4RainyMaple <z1975917270@qq.com>
2026-09-16 14:48:27 +0800

    Create 16.最接近的三数之和.cpp

---
+++ 数组/16.最接近的三数之和.cpp
@@ -0,0 +1,37 @@
+//依旧双指针法
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        sort(nums.begin(), nums.end());
+        int len = (int) nums.size();
+        int closestSum = nums[0] + nums[1] + nums[2];
+        int thisSum;
+
+        for ( int i = 0; i < len - 2; ++i )
+        {
+            if ( i > 0 && nums[i] == nums[i-1] )
+                continue;
+            int left = i + 1;
+            int right = len - 1;
+            while ( left < right )
+            {
+                thisSum = nums[i] + nums[left] + nums[right];
+
+                if ( abs ( target - thisSum ) < abs ( closestSum - target ) )
+                    closestSum = thisSum;
+                
+                //大了就让右指针左移，小了就让左指针右移
+                if ( thisSum < target )
+                    ++left;
+
+                else if ( thisSum > target )
+                    --right;
+
+                else 
+                    return target;
+            }
+        }
+        return closestSum;
+    }
+};
