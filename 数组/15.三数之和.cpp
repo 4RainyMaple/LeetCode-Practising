2a3d7b5018436e694bc4067831da1960f35fca70
4RainyMaple <z1975917270@qq.com>
2026-09-21 11:13:57 +0800

    Update 15.三数之和.cpp

--- 数组/15.三数之和.cpp
+++ 数组/15.三数之和.cpp
@@ -1,45 +1,55 @@
-/*
-我们先从三重循环入手（尽管这肯定不是最好的算法）
-不重复意味着第二重循环的数与第一重不同，第三重循环的数与第二重循环不同
-如果每次都走一轮太麻烦了，最好的办法就是先排序（O(NlogN)），如此数组是不减的
-排序好后再循环，只要发现枚举的数字与上一个相同就直接往下走
-
-在第一重循环内，注意到如果第二个数增加，那第三个数就要减小
-这种情况下可以考虑双指针法，把后两重循环合并为一重循环
-记第二个下标为left,第三个数下标为right
-让left循环枚举，每次right从最右端往左走，只要三数之和为0就停止，进入下一个循环
-*/
+//双指针法
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) 
-    {
-        vector<vector<int>> ret;
-        sort( nums.begin(), nums.end() );
-        int len = (int)nums.size();
-      
-        for ( int i = 0; i < len; ++i )
-        {
-            if ( i > 0 && nums[i] == nums[i-1] )  //注意别上来就查重，会越界访问
-                continue;
-            
-            int right = len - 1;
-            for ( int left = i + 1; left < len; ++left )
-            {
-                if ( left > i + 1 && nums[left] == nums[left-1] )
-                    continue;
-              
-                while ( left < right && nums[i] + nums[left] + nums[right] > 0 )
-                {
-                    --right;
-                }
-                if ( left == right )  //别让这两交错，否则重复
-                    break;
-              
-                if ( nums[i] + nums[left] + nums[right] == 0 )
-                    ret.push_back({nums[i],nums[left],nums[right]});
-            }
-        }
-        return ret;
-    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int len = (int)nums.size();
+        vector<vector<int>> ret;
+        sort( nums.begin(), nums.end() );
+
+        for ( int i = 0; i < len - 2; ++i )
+        {
+            //查重
+            if ( i > 0 && nums[i] == nums[i-1] )
+                continue;
+            //考察最大最小值，省略不必要的循环
+            if ( nums[i] + nums[i+1] + nums[i+2] > 0 )
+                break;
+
+            if ( nums[i] + nums[len-1] + nums[len-2] < 0 )
+                continue;
+
+            int left = i + 1;
+            int right = len - 1;
+            int sum;
+
+            while ( left < right )
+            {
+                sum = nums[i] + nums[left] + nums[right];
+                if ( sum == 0 )
+                {
+                    ret.push_back({ nums[i], nums[left], nums[right] });
+                    //注意left和right也要查重
+                    do{
+                        ++left;
+                    }while( left < right && nums[left] == nums[left-1] );
+
+                    do{
+                        --right;
+                    }while( left < right && nums[right] == nums[right+1] );
+                }
+
+                else if ( sum < 0 )
+                    do{
+                        ++left;
+                    }while( left < right && nums[left] == nums[left-1] );
+
+                else
+                    do{
+                        --right;
+                    }while( left < right && nums[right] == nums[right+1] );
+            }
+        }
+        return ret;
+    }
 };
