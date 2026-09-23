7d932ae68f9a674a0e399b8efd659f6cf174c7bd
4RainyMaple <z1975917270@qq.com>
2026-09-23 14:15:50 +0800

    Update 1.两数之和.cpp

--- 数组/1.两数之和.cpp
+++ 数组/1.两数之和.cpp
@@ -1,19 +1,19 @@
-//哈希表的算法待补充
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        for ( int i = 0; i < nums.size(); ++i )
-          //注意这里返回值是int，所以i和j直接定义为int
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //建立一个哈希表，key值装nums的元素，value值装nums元素的下标
+        map < int, int > hashTable;
+        int len = (int)nums.size();
+        //只需一层循环，时间复杂度O(N)
+        for ( int i = 0; i< len; ++i )
         {
-            for ( int j = i + 1; j < nums.size(); ++j )
-            {
-                if ( nums[i] + nums[j] == target )
-                {
-                    return { i, j };
-                }
-            }
-        }
+            //找找哈希表里面有没有符合条件的元素，若有则返回当前下标和对应下标
+            auto itr = hashTable.find( target - nums[i] );
+            if ( itr != hashTable.end() )
+                return { i, hashTable[target - nums[i]] };
+            //否则把当前元素集下标存进哈希表
+            hashTable.insert( {nums[i], i} );
+        } 
         return {};
     }
 };
