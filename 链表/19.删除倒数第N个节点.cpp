12c043c970829e84d533e5c7345bf6e31ccdda31
4RainyMaple <z1975917270@qq.com>
2026-09-26 10:02:52 +0800

    Create 19.删除倒数第N个节点.cpp

---
+++ 链表/19.删除倒数第N个节点.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * dummy = new ListNode(0,head);    //虚构的头节点的前一个节点
+        int len = getLength(head);
+
+        ListNode * thisNode = dummy;    //从dummy开始，才能处理删除头节点的问题
+        for ( int i = 1; i <= len - n; i++ )
+            thisNode = thisNode->next;
+
+        thisNode->next = thisNode->next->next;
+        return dummy->next;
+    }
+
+private:
+    int getLength(ListNode* head)
+    {
+        int len = 1;
+
+        while ( head->next != nullptr )
+        {
+            head = head->next;
+            len++;
+        }
+        return len;
+    }
+};
